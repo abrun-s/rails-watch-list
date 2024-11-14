0bdc03f224b62da282b198dec38f742a2f88ff05
adam brunson <adam.brunson@yahoo.com>
2024-11-14 05:44:30 -0800

    needs graphics

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+
+  def show
+    @list = List.find(params[:id])
+
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    # debugger
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @list = List.new
+
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end

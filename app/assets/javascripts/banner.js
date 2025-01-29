b55c5d5b77dcb6f02955abdd41f1db16f044be79
adam brunson <adam.brunson@yahoo.com>
2025-01-28 23:02:00 -0800

    scrolling bander is work in show page, no loop

---
+++ app/assets/javascripts/banner.js
@@ -0,0 +1,15 @@
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const bannerTrack = document.querySelector(".banner-track");
+
+//   // if (bannerTrack) {
+//     // Pause scrolling on hover
+//     bannerTrack.addEventListener("mouseover", () => {
+//       bannerTrack.style.animationPlayState = "paused";
+//     });
+
+//     bannerTrack.addEventListener("mouseleave", () => {
+//       bannerTrack.style.animationPlayState = "running";
+//     });
+//   // }
+// });
